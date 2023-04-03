657e948c2b5e149ad3281841bf795d83340b6ebf
Sean Hayes <hayessea000@outlook.com>
2023-04-03 16:39:00 -0500

    event listener finds out if its right or wrong

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,12 +7,14 @@ var question3 = {q:"q3", q1:"1", q2:"2",q3:"3", q4:"4"}
 var qs =[question1,question2,question3] 
 
 quizEvent.addEventListener("click", function(event) {
-    console.log("box")
     var element = event.target;
     if (element.matches(".answer")) {
-        console.log("yes")
-        if (element.matches("#correct")){
+        var status = element.getAttribute("data-status")
+        if (status==="correct"){
             console.log("cor")
+        }else {
+            console.log("in")
         }
+        // next question
     }
 })
