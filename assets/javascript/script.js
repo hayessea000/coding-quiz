caaa3c3a0dff1bee6ed2746aea8141d421080d16
Sean Hayes <hayessea000@outlook.com>
2023-04-03 15:31:41 -0500

    started adding js

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+var quizEvent = document.getElementById("quizbox");
+
+var qs = {
+    question1:{q1:"1", q2:"2",q3:"3", q4:"4"},
+    question2:{q1:"1", q2:"2",q3:"3", q4:"4"},
+    question3:{q1:"1", q2:"2",q3:"3", q4:"4"},
+}
