c171d91f2a432a3d2e0a0cddf722ea3d2645bf2f
Sean Hayes <hayessea000@outlook.com>
2023-04-03 17:42:34 -0500

    made it so the questions will rotate through

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,18 +6,20 @@ var a3 = document.querySelector("#a3");
 var a4 = document.querySelector("#a4");
 
 var question1 = {q:"q1", answerLine1:"1", answerLine2:"2",answerLine3:"3", answerLine4:"4"}
-var question2 = {q:"q2", answerLine1:"1", answerLine2:"2",answerLine3:"3", answerLine4:"4"}
-var question3 = {q:"q3", answerLine1:"1", answerLine2:"2",answerLine3:"3", answerLine4:"4"}
+var question2 = {q:"q2", answerLine1:"yes", answerLine2:"2",answerLine3:"3", answerLine4:"4"}
+var question3 = {q:"q3", answerLine1:"1", answerLine2:"2",answerLine3:"3", answerLine4:"q4"}
 var qs =[question1,question2,question3] 
-
-var nextQ = function(){
-    qHead.textContent= question1.q;
-    a1.textContent= question1.answerLine1;
-    a2.textContent= question1.answerLine2;
-    a3.textContent= question1.answerLine3;
-    a4.textContent= question1.answerLine4;
+var questionCount = 0
+var runQuestions = function(){
+    
+    qHead.textContent= qs[questionCount].q;
+    a1.textContent= qs[questionCount].answerLine1;
+    a2.textContent= qs[questionCount].answerLine2;
+    a3.textContent= qs[questionCount].answerLine3;
+    a4.textContent= qs[questionCount].answerLine4;
+    questionCount++
+    console.log(questionCount)
 }
-
 
 
 
@@ -34,7 +36,6 @@ quizEvent.addEventListener("click", function(event) {
         }else {
             console.log("in")
         }
-        // next question
-        nextQ()
+        runQuestions()
     }
 })
