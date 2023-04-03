89562bc237e933a36f494312107cd522bf4c693a
Sean Hayes <hayessea000@outlook.com>
2023-04-03 16:24:47 -0500

    added event listener

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,18 @@
-var quizEvent = document.getElementById("quizbox");
+var quizEvent = document.querySelector("article");
 
-var qs = {
-    question1:{q1:"1", q2:"2",q3:"3", q4:"4"},
-    question2:{q1:"1", q2:"2",q3:"3", q4:"4"},
-    question3:{q1:"1", q2:"2",q3:"3", q4:"4"},
-}
+
+var question1 = {q:"q1", q1:"1", q2:"2",q3:"3", q4:"4"}
+var question2 = {q:"q2", q1:"1", q2:"2",q3:"3", q4:"4"}
+var question3 = {q:"q3", q1:"1", q2:"2",q3:"3", q4:"4"}
+var qs =[question1,question2,question3] 
+
+quizEvent.addEventListener("click", function(event) {
+    console.log("box")
+    var element = event.target;
+    if (element.matches(".answer")) {
+        console.log("yes")
+        if (element.matches("#correct")){
+            console.log("cor")
+        }
+    }
+})
