3dc12d8291eeaf76c6e78aac8ac42aee5535ec12
Sean Hayes <hayessea000@outlook.com>
2023-04-06 18:38:12 -0500

    made the reset work the way i want it to work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,14 +8,14 @@ var answer3 = document.querySelector("#a3");
 var answer4 = document.querySelector("#a4");
 var quizScreen = document.querySelector("#quizBlock");
 var hiScoreScreen = document.querySelector("#hiBlock");
-var quizStatus= "start"
-var answerStatus
-var score= 0
-var element
-var timeLeft
-var timer
-var questionCount
-var saved
+var quizStatus= "start";
+var answerStatus;
+var score= 0;
+var element;
+var timeLeft;
+var timer;
+var questionCount;
+var saved;
 var question1 = {
     q:"which is a color?",
     a1:{name: "blue", status:"correct"},
@@ -40,19 +40,7 @@ var question3 = {
     a4:{name: "apple", status:"incorrect"},
 }
 
-var questions =[question1,question2,question3] 
-
-var loadScore = function(){
-    saved = JSON.parse(localStorage.getItem("hiScores"));
-    if(saved!== null){
-        for(let i = 0; i< saved.length; i++){
-            let myscore= document.createElement("li")
-            myscore.textContent=saved[i]
-            leaderboard.appendChild(myscore);
-    }}else{
-        saved=[]
-    }
-}
+var questions =[question1,question2,question3]; 
 
 var startTimer= function () {
     timeLeft= 15;
@@ -62,7 +50,7 @@ var startTimer= function () {
         timeEl.textContent = timeLeft + " seconds left to finish.";
         if(timeLeft <= 0) {
             clearInterval(timer);
-            hiScoreChanger()
+            displaySavedScores();
         }
     }, 1000);
 }
@@ -71,91 +59,100 @@ var runQuestions = function(){
     questionHeader.textContent= questions[questionCount].q;
     answer1.textContent= questions[questionCount].a1.name;
     if(questions[questionCount].a1.status == "correct"){
-        answer1.setAttribute("data-status", "correct")
+        answer1.setAttribute("data-status", "correct");
     }
     answer2.textContent= questions[questionCount].a2.name;
     if(questions[questionCount].a2.status == "correct"){
-        answer2.setAttribute("data-status", "correct")
+        answer2.setAttribute("data-status", "correct");
     }
     answer3.textContent= questions[questionCount].a3.name;
     if(questions[questionCount].a3.status == "correct"){
-        answer3.setAttribute("data-status", "correct")
+        answer3.setAttribute("data-status", "correct");
     }
     answer4.textContent= questions[questionCount].a4.name;
     if(questions[questionCount].a4.status == "correct"){
         answer4.setAttribute("data-status", "correct")
     }
-    questionCount++
+    questionCount++;
 }
 
 var checkAnswer= function(){
     if (answerStatus==="correct"){
-        score++
-        element.setAttribute("data-status", "incorrect")
+        score++;
+        element.setAttribute("data-status", "incorrect");
     }else {
-        timeLeft= timeLeft-5
+        timeLeft= timeLeft-5;
     }
     if (questionCount!== questions.length){
-        runQuestions()
+        runQuestions();
     }else {
         clearInterval(timer);
-        hiScoreChanger()
+        displaySavedScores();
     }
 }
 
-var hiScoreChanger= function(){
+function displaySavedScores(){
+
     quizStatus= "hiScore";
     quizScreen.setAttribute("class", "hidden") ;  
     hiScoreScreen.setAttribute("class", "hiScore");
     var setName = prompt("Please enter your name");
-    var newScore = document.createElement("li");
-    var scoreName= setName +" "+ score
-    newScore.textContent = scoreName;  
-    leaderboard.appendChild(newScore);
+    var scoreName= setName +" "+ score;
+    saved = JSON.parse(localStorage.getItem("hiScores"));
+    if(saved== null){
+        saved=[];
+    }
     saved.push(scoreName);
     localStorage.setItem("hiScores", JSON.stringify(saved));
+    leaderboard.innerHTML = "";
+    if(saved!== null){
+        for(var i = 0; i< saved.length; i++){
+            var myscore= document.createElement("li");
+            myscore.textContent=saved[i];
+            leaderboard.appendChild(myscore);
+        }
+    }
 }
 
 var startUp =function(){
-    questionCount = 0
-    score = 0
-    quizStatus= "quiz"
-    startTimer()
-    runQuestions()
-    
+    questionCount = 0;
+    score = 0;
+    quizStatus= "quiz";
+    startTimer();
+    runQuestions();
 }
 
 var retake = function(){
     quizScreen.setAttribute("class", "box") ;   
     hiScoreScreen.setAttribute("class", "hidden");
-    startUp()
+    startUp();
+}
+
+var reset = function(){
+    leaderboard.innerHTML = "";
 }
 
 quizEvent.addEventListener("click", function(event) {
     if(quizStatus=="start"){
         element = event.target;    
         if (element.matches("#start")){
-            startBox.remove()
-            loadScore()
-            startUp()
+            startBox.remove();
+            startUp();
         }
     }if(quizStatus=="quiz"){
         element = event.target;
         if (element.matches(".answer")) {
-             answerStatus = element.getAttribute("data-status")
-             checkAnswer()
+             answerStatus = element.getAttribute("data-status");
+             checkAnswer();
         }
     }if(quizStatus=="hiScore"){
         element = event.target;
         if (element.matches("#retake")) {
-             retake()
+             retake();
         }if (element.matches("#reset")) {
-            if (saved.length!==0){
-                var tester =document.querySelector("li")
-                leaderboard.removeChild(tester)
-            }
-            saved.shift()
+            saved= [];
             localStorage.setItem("hiScores", JSON.stringify(saved));
+            reset();
        }
     }
 })
